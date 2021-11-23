76c7c7572c149d7419129b0421180f62ddba9f89
Talha636 <85548877+Talha636@users.noreply.github.com>
2021-11-22 21:48:52 -0500

    offline capabilities created

---
+++ public/db.js
@@ -0,0 +1,52 @@
+let db;
+let budgetVersion;
+
+const request = indexedDB.open('budget', 1);
+
+request.onupgradeneeded = (event) => {
+    const db = event.target.result;
+    db.createObjectStore('budget', { autoIncrement: true });
+};
+
+request.onerror = (error) => {
+    console.log(error.target.errorCode);
+};
+
+request.onsuccess = (success) => {
+    db = success.target.result;
+    if (navigator.onLine) {
+        checkDatabase();
+    }
+};
+
+checkDatabase = () => {
+    const transaction = db.transaction(['budget'], 'readwrite');
+    const store = transaction.objectStore('budget');
+    const getAll = store.getAll();
+
+    getAll.onsuccess = () => {
+        if (getAll.result.length > 0) {
+            fetch('api/transaction/bulk', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/jaon, text/plain, */*',
+                    'Content-Type': 'application/json',
+                },
+            }).then((res) => res.json())
+            .then(() => {
+                const transaction = db.transaction(['budget'], 'readwrite');
+                const store = transaction.objectStore('budget');
+                store.clear();
+            })
+        }
+    }
+}
+
+const saveRecord = (record) => {
+    const transaction = db.transaction(['budget'], 'readwrite');
+    const store = transaction.objectStore('budget');
+    store.clear(record);
+}
+
+window.addEventListener('online', chechDatabase);
